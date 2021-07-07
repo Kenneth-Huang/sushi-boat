213f07ba5dfd52a857ca58e77f92d2b220e1ee30
Kenneth <Kenneth.p.huang@gmail.com>
2021-07-08 00:25:42 +1000

    Add styled-components and google fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+body{
+  font-family: 'Roboto', sans-serif;
+}
+h1, h2, h3, h4, h5, h6{
+  font-family: 'Ubuntu', sans-serif;
+}
+`;
+
 function App() {
-	return <div>Hello world</div>;
+	return (
+		<>
+			<GlobalStyles />
+			<h1>Welcome</h1>
+			<div>Hello world</div>
+		</>
+	);
 }
 
 export default App;
